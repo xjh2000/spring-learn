5458147ff619849da5c18caddd4562b69009e8d8
xjh <934622645@qq.com>
2022-10-22 10:54:49 +0800

    IOC xml configure DI

--- src/main/java/org/xjh/spring/springLearn/service/impl/BookServiceImpl.java
+++ src/main/java/org/xjh/spring/springLearn/service/impl/BookServiceImpl.java
@@ -1,16 +1,20 @@
 package org.xjh.spring.springLearn.service.impl;
 
 import org.xjh.spring.springLearn.dao.BookDao;
-import org.xjh.spring.springLearn.dao.impl.BookDaoImpl;
 import org.xjh.spring.springLearn.service.BookService;
 
 public class BookServiceImpl implements BookService {
-    private final BookDao bookDao = new BookDaoImpl();
+    //    private final BookDao bookDao = new BookDaoImpl();
+    private BookDao bookDao;
 
     @Override
     public void save() {
         System.out.println("service is save ");
         bookDao.save();
 
+    }
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
