6b0bb6e4bf744e3e2d9d0c5c7ccc2525cfb8aeaf
xjh <934622645@qq.com>
2022-10-23 09:50:00 +0800

    request mapping

--- src/main/java/org/xjh/spring/springLearn/controller/UserController.java
+++ src/main/java/org/xjh/spring/springLearn/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/save")
