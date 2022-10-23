19426d5660702e1e1fdc48bad438b4694eae515a
xjh <934622645@qq.com>
2022-10-23 11:08:39 +0800

    request json support

--- src/main/java/org/xjh/spring/springLearn/config/SpringMvcConfig.java
+++ src/main/java/org/xjh/spring/springLearn/config/SpringMvcConfig.java
@@ -2,8 +2,10 @@ package org.xjh.spring.springLearn.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("org.xjh.spring.springLearn.controller")
+@EnableWebMvc
 public class SpringMvcConfig {
 }
--- src/main/java/org/xjh/spring/springLearn/controller/UserController.java
+++ src/main/java/org/xjh/spring/springLearn/controller/UserController.java
@@ -1,9 +1,12 @@
 package org.xjh.spring.springLearn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.xjh.spring.springLearn.domain.User;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -30,4 +33,17 @@ public class UserController {
         return "success";
     }
 
+    @RequestMapping("/listParamForJson")
+    @ResponseBody
+    public String listParamForJson(@RequestBody List<String> likes) {
+        System.out.println(likes);
+        return "success";
+    }
+
+    @RequestMapping("/pojoParamForJson")
+    @ResponseBody
+    public String pojoParamForJson(@RequestBody User user) {
+        System.out.println(user);
+        return "success";
+    }
 }
