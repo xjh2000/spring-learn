0c11eddbd4d6fa58ef2d2effec96326cd27f7ecd
xjh <934622645@qq.com>
2022-10-23 08:33:49 +0800

    spring mvc start up

---
+++ src/main/java/org/xjh/spring/springLearn/config/ServletContainersInitConfig.java
@@ -0,0 +1,25 @@
+package org.xjh.spring.springLearn.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(SpringMvcConfig.class);
+        applicationContext.refresh();
+        return applicationContext;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
+}
---
+++ src/main/java/org/xjh/spring/springLearn/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package org.xjh.spring.springLearn.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.xjh.spring.springLearn")
+public class SpringMvcConfig {
+}
